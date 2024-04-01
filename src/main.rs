b3b26db66c63a5727e6d86e62c694ff9ef3456b3
Saksham Tandon <54137431+saksham1236@users.noreply.github.com>
2024-04-01 07:50:09 +0000

    Adding not so elegant solution

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,31 @@
-fn sort_usernames<T: AsRef<str>>(usernames: &mut Vec<T>) {
-    todo!();
+fn sort_usernames<T: AsRef<str> + std::clone::Clone>(usernames: &mut Vec<T>) {
+    for _ in 0..usernames.len() {
+        let mut i = 0;
+        while i < usernames.len() - 1 {
+            let name = usernames[i].as_ref().to_lowercase();
+            let name_next: String = usernames[i + 1].as_ref().to_lowercase();
+            let temp = if name.len() < name_next.len() {
+                name.len()
+            } else {
+                name_next.len()
+            };
+            for j in 0..temp {
+                if name.chars().nth(j) == name_next.chars().nth(j) {
+                    continue;
+                } else {
+                    if name.chars().nth(j) > name_next.chars().nth(j) {
+                        usernames.swap(i, i + 1)
+                    }
+                    break;
+                }
+            }
+            i += 1;
+        }
+    }
 }
 
 fn main() {
-    let mut users = vec!["Todd", "Amy", "mike99", "Jennifer", "alison"];
+    let mut users = vec!["@Todd", "$Amy", "mike99", "Jennifer", "alison"];
 
     println!("unsorted: {:?}", &users);
     sort_usernames(&mut users);
