90f0c7c3f6a4e1cd7253a57d39173e7ff6f9b04c
Saksham Tandon <54137431+saksham1236@users.noreply.github.com>
2024-04-20 07:27:35 +0000

    Adding solution

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
+use regex::Regex;
 use std::fmt::Display;
 use std::str::FromStr;
+use std::u8;
 
 #[derive(Debug, PartialEq)]
-struct Rgb; // TODO: design data structure
-
+struct Rgb {
+    r: u8,
+    g: u8,
+    b: u8,
+} // TODO: design data structure
+#[derive(Debug, PartialEq, Eq)]
+struct ParseError;
 trait RgbChannels {
     fn r(&self) -> u8;
 
@@ -13,11 +20,37 @@ trait RgbChannels {
 }
 
 impl RgbChannels for Rgb {
-    // TODO: implement trait
+    fn b(&self) -> u8 {
+        self.b
+    }
+    fn g(&self) -> u8 {
+        self.g
+    }
+    fn r(&self) -> u8 {
+        self.r
+    }
 }
 
 impl FromStr for Rgb {
-    // TODO: implement trait
+    type Err = ParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let pattern = Regex::new("^#([a-f0-9]{6})$").unwrap();
+        if !pattern.is_match(s) {
+            Err(ParseError)
+        } else {
+            let strip_string = s.trim_start_matches("#");
+            let r_val = u8::from_str_radix(&strip_string[0..2], 16).unwrap();
+            let g_val = u8::from_str_radix(&strip_string[2..4], 16).unwrap();
+            let b_val = u8::from_str_radix(&strip_string[4..6], 16).unwrap();
+            let channels = Rgb {
+                r: r_val,
+                b: g_val,
+                g: b_val,
+            };
+            return Ok(channels);
+        }
+    }
 }
 
 impl Display for Rgb {
@@ -27,7 +60,7 @@ impl Display for Rgb {
 }
 
 fn main() {
-    // 
+    //
 }
 
 #[test]
@@ -43,19 +76,19 @@ fn every_color() {
 
 #[test]
 #[should_panic]
-fn too_short () {
+fn too_short() {
     let _: Rgb = "1234".parse().unwrap();
 }
 
 #[test]
 #[should_panic]
-fn not_a_hex_code () {
+fn not_a_hex_code() {
     let _: Rgb = "?".parse().unwrap();
 }
 
 #[test]
 #[should_panic]
-fn invalid_literals () {
+fn invalid_literals() {
     let _: Rgb = "?".parse().unwrap();
 }
 
@@ -70,4 +103,3 @@ fn no_leading_hash() {
 fn out_of_bounds() {
     let _: Rgb = "00gg00".parse().unwrap();
 }
-
